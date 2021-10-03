5c2c4918658a7f7ebdfdd265187cecf2d99e938b
NguyenTruongSon0101 <sonn6272@gmail.com>
2021-10-03 14:25:50 +0700

    bổ sung Lab3Bai3

---
+++ src/com/sonnt2005110011/tuan03/Lab3Bai3.java
@@ -0,0 +1,35 @@
+package com.sonnt2005110011.tuan03;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Lab3Bai3 {
+    public static void main(String[] args) {
+    int[] soNguyen = new int [5];
+    //nhập mảng
+    Scanner nhapMang = new Scanner(System.in);
+    for(int i = 0; i < soNguyen.length; i++){
+        System.out.println("số thứ "+(i+1));
+        soNguyen[i] = nhapMang.nextInt();
+        
+    }
+    //xuất mảng vừa nhập
+    System.out.print("mảng vừa nhập");
+    for (int i : soNguyen) {
+        System.out.print("\t"+i);
+    }
+    //xuất mảng theo thứ tự tăng dần
+    System.out.print("\nmảng sau khi sắp xếp:");
+    Arrays.sort(soNguyen);
+    for (int i : soNguyen) {
+        System.out.print("\t"+i);
+    }
+    //tìm giá trị nhỏ nhất
+    int min = soNguyen[0];
+    for(int i = 0; i< soNguyen.length; i++){
+        min = Math.min(min, soNguyen[i]);
+    }
+    System.out.print("\nPhần tử có giá trị nhỏ nhất:"+min);
+    
+}
+}
