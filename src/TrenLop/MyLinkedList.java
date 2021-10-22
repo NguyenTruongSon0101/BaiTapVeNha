0080e30cef558e8fd7912096a80d063ae0c4431c
NguyenTruongSon0101 <sonn6272@gmail.com>
2021-10-22 15:21:20 +0700

    bo sung tuan 7

---
+++ src/TrenLop/MyLinkedList.java
@@ -0,0 +1,108 @@
+package TrenLop;
+
+public class MyLinkedList {
+    Node head = null;
+    Node tail = null;
+
+//thêm vào cuối danh sách
+void add(int data){
+    Node newNode = new Node(data);
+
+    if(head == null){
+        head = newNode;
+        tail = newNode;
+    }else{
+        tail.next = newNode;
+        tail = newNode;
+    }
+}
+//in danh sách
+void print(){
+    Node current = head;
+
+    if(head == null){
+        System.out.println("Danh sách rỗng");
+        return;
+    }
+    System.out.println("các nút trong danh sách");
+    while(current != null){
+        System.out.println(current.data);
+        current = current.next;
+    }
+}
+//thêm vào đầu danh sách
+void addHead(int data){
+    Node newNode = new Node(data);
+    if(head == null){
+        head = newNode;
+        tail = newNode;
+    }else{
+        newNode.next = head;
+        head = newNode;
+    }
+}
+//thêm vào vị trí trước số cần tìm
+void addAfter(int soCanTim,int soCanThem){
+    int dem = 0;
+    Node tam = head;
+    Node truocTam = head;
+    while(tam != null){
+        if(tam.data == soCanTim)
+            break;
+        tam = tam.next;
+        dem++;
+        if(dem>1)
+            truocTam = truocTam.next;
+        
+    }
+    Node them = new Node();
+    them.data = soCanThem;
+    
+            if(dem == 0){
+                addHead(soCanThem);
+            }else{
+                 them.next = tam;
+                 truocTam.next = them;
+                 }
+    
+    
+    }
+    //xoá đầu danh sách
+    void deleteHead(){
+        head = head.next;
+    }
+    //xóa cuối danh sách
+    void deleteTail(){
+        Node tam = head;
+        while(tam != null){
+            if(tam.next == tail){
+                tam.next = null;
+                tail = tam;
+                break;
+            }
+            tam = tam.next;
+        }
+    }
+    //xóa số bất kỳ trong danh sách
+    void delete(int soCanXoa){
+        Node tam = head;
+        
+        
+        while(tam != null){
+            if(tam.next.data == soCanXoa)
+                break;
+            tam = tam.next;
+        }
+        tam.next = tam.next.next;
+    }
+    //sửa thông tin của 1 phần tử
+    void fix(int soCanSua,int soDaSua){
+        Node tam = head;
+        while(tam != null){
+            if(tam.data == soCanSua)
+                break;
+            tam = tam.next;
+        }
+        tam.data = soDaSua;
+    }
+}
---
+++ src/TrenLop/MyLinkedListTestDrive.java
@@ -0,0 +1,41 @@
+package TrenLop;
+
+public class MyLinkedListTestDrive {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(4);
+        myLinkedList.add(8);
+        myLinkedList.add(5);
+        System.out.println("danh sách ban đầu");
+        myLinkedList.print();
+
+        System.out.println("Sau khi thêm 1 nút vào cuối : 7");
+        myLinkedList.add(7);
+        myLinkedList.print();
+
+        System.out.println("Sau khi thêm vào đầu danh sách : 8");
+        myLinkedList.addHead(8);
+        myLinkedList.print();
+
+        System.out.println("Thêm vào trước số cần tìm trong danh sách : thêm 7 trước 4");
+        myLinkedList.addAfter(4,7);
+        myLinkedList.print();
+
+        System.out.println("xóa nút đầu danh sách");
+        myLinkedList.deleteHead();
+        myLinkedList.print();
+
+        System.out.println("xóa cuối danh sách");
+        myLinkedList.deleteTail();
+        myLinkedList.print();
+
+        System.out.println("xóa vị số bất kỳ trong danh sách:  4");
+        myLinkedList.delete(4);
+        myLinkedList.print();
+
+        System.out.println("sửa số 8 thành số 1");
+        myLinkedList.fix(8, 1);
+        myLinkedList.print();
+    }
+
+}
